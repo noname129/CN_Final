4b9fdcff5061a41bc79e5186644c71b2b909b67d
nonami <noname129@naver.com>
2019-11-13 01:37:30 +0900

    로비 프로토콜 정의

--- common.py
+++ common.py
@@ -1,9 +1,22 @@
 import struct
+import enum
 
 # constant fields
 SET_USER_NAME_REQUEST = 1
 GET_USER_LIST_REQUEST = 2
 GET_USER_LIST_RESPONSE = 3
+
+
+class Protocols(enum.IntEnum):
+    set_user_name_request = enum.auto()
+    send_user_list = enum.auto()
+    create_room_request = enum.auto()
+    create_room_response = enum.auto()
+    join_room_request = enum.auto()
+    join_room_response = enum.auto()
+    update_guest_name = enum.auto()
+    start_game_request = enum.auto()
+    start_game = enum.auto()
 
 
 def create_packet(packet):
@@ -30,7 +43,7 @@ class GetUserListRequest:
         self.values = (GET_USER_LIST_REQUEST,)
 
 
-class GetUserListResponse:
+class SendUserList:
     @classmethod
     def var_fmt(cls, user_count):
         return 'I I' + ' 16s' * user_count
@@ -39,3 +52,45 @@ class GetUserListResponse:
         user_count = len(names)
         self.values = (GET_USER_LIST_RESPONSE, user_count) + names
         self.fmt = 'I I' + ' 16s' * user_count
+
+
+class CreateRoomRequest:
+    fmt = 'I I I'
+
+    def __init__(self, board_width, board_height):
+        self.values = (int(Protocols.create_room_request), board_width, board_height)
+
+
+class CreateRoomRequest:
+    fmt = 'I I I I'
+
+    def __init__(self, code, board_width, board_height):
+        self.values = (int(Protocols.create_room_request), code, board_width, board_height)
+
+
+class JoinRoomRequest:
+    fmt = 'I I'
+
+    def __init__(self, room_number):
+        self.values = (int(Protocols.join_room_request), room_number)
+
+
+class JoinRoomResponse:
+    fmt = 'I I 16s I I'
+
+    def __init__(self, room_number, opponent_name, board_width, board_height):
+        self.values = (int(Protocols.join_room_response), room_number, opponent_name, board_width, board_height)
+
+
+class UpdateGuestName:
+    fmt = 'I 16s'
+
+    def __init__(self, opponent_name):
+        self.values = (int(Protocols.update_guest_name), opponent_name)
+
+
+class StartGame:
+    fmt = 'I'
+
+    def __init__(self):
+        self.values = (int(Protocols.start_game),)
