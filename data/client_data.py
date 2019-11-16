2e72f89baa3cb0be462018b4972e8670e99b1463
nonami <noname129@naver.com>
2019-11-16 16:46:08 +0900

    GameInstance 클래스 업데이트

--- data/client_data.py
+++ data/client_data.py
@@ -10,13 +10,13 @@ class GameInstance:
     Client's view of the server's GameInstance.
     Immutable.
     '''
-    def __init__(self):
+    def __init__(self, instance_id, players, max_players, name, field_size, mine_prob):
         # TODO actually make this class useful
-        self.players=("asdf","qwer","zxcv")
-        self.max_players=4
-        self.joinable=True
-        self.instance_id=123
-        self.name="A game!"
-        self.field_size=(40,20)
-        self.mine_prob=0.05
+        self.instance_id = instance_id
+        self.players = players
+        self.max_players = max_players
+        self.joinable = max_players - len(players) > 0
+        self.name = name
+        self.field_size = field_size
+        self.mine_prob = mine_prob
 
--- data/server_data.py
+++ data/server_data.py
@@ -8,8 +8,8 @@ from data import client_data
 
 
 class Player:
-    name="playerrr"
-    address="0.0.0.0:0"
+    def __init__(self, name):
+        self.name = name
 
 class GameState(enum.Enum):
     IN_PROGRESS=enum.auto()
@@ -18,15 +18,25 @@ class GameState(enum.Enum):
     FINISHED=enum.auto()
 
 class GameInstance:
-    players=[]
-    field_dimension = (40, 20)
-    mine_probability=0.05
-    max_players = 4
-    state=GameState.WAIT_FOR_PLAYERS
-    instance_id=123
+    def __init__(self, instance_id, field_dimension=(40, 20), mine_probability=0.05, max_players=4):
+        self.player_threads = []
+        self.instance_id = instance_id
+        self.state = GameState.WAIT_FOR_PLAYERS
+        self.field_dimension = field_dimension
+        self.mine_probability = mine_probability
+        self.max_players = max_players
 
     def convert_to_client_version(self):
-        gi= client_data.GameInstance()
-        # TODO implement
+        gi = client_data.GameInstance(
+            instance_id=self.instance_id,
+            players=tuple([player.player_info.name for player in self.player_threads]),
+            max_players=self.max_players,
+            name=self.player_threads[0].player_info.name if len(self.player_threads) > 0 else "Ghost Room",
+            field_size=self.field_dimension,
+            mine_prob=self.mine_probability
+        )
+        return gi
 
+    def is_joinable(self):
+        return self.state == GameState.WAIT_FOR_PLAYERS and self.max_players - len(self.player_threads) > 0
 
